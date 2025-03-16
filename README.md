64aab1cdcbfca291bd05130d93c0c28c5ea520c2
Seasawher <tellagoodstory@icloud.com>
2025-03-16 12:27:23 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,1 @@
+# LeaningIn2025
---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package "LeaningIn2025" where
+  version := v!"0.1.0"
+
+@[default_target]
+lean_lib «LeaningIn2025» where
+  -- add library configuration options here
